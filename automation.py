a69e3c607ab03c68a50a45d4b4523416ec7d9e90
Jonas H <hanot.jonas@gmail.com>
2021-04-22 12:11:50 +0200

    Add initial setup for testing automation

--- automation.py
+++ automation.py
@@ -5,5 +5,12 @@ browser = webdriver.Firefox(executable_path='./geckodriver.exe')
 browser.maximize_window()
 
 browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+show_msg_button = browser.find_element_by_class_name('btn-default')
 
-assert "show message" in browser.page_source
+assert "Show Message" in browser.page_source
+
+user_message = browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('COOOOOOOOOOOL')
+
+browser.close()
